e8957adabf290a866668e2ef5f2790260bf37557
Dolf Barr <dolf.barr@gmail.com>
2021-08-09 12:48:02 +0300

    chore: add update notifier

--- src/hooks/init/update.ts
+++ src/hooks/init/update.ts
@@ -1,13 +1,12 @@
 import {Hook} from '@oclif/config'
 import updateNotifier from 'update-notifier'
 
-import pkg from '../../../package.json'
 import {Alerts} from '../../utils/messages'
 import {Logger, update} from '../../utils/renderer'
 
 export const notify: Hook<'init'> = async function () {
   const {fav} = new Logger(this.log)
-  const notifier = updateNotifier({pkg})
+  const notifier = updateNotifier({pkg: {name: this.config.name, version: this.config.version}})
 
   if (notifier.update) {
     const {current, latest} = notifier.update
